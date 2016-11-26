a190ac962dc38206195f96e6d8491a69fc20d493
lightless <lightless@foxmail.com>
2016-11-26 15:14:44 +0800

    add new models

--- apps/engine_app/models.py
+++ apps/engine_app/models.py
@@ -1,5 +1,23 @@
+#!/usr/bin/env python2
+# coding: utf8
+
 from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+__author__ = 'lightless'
+__email__ = 'root@lightless.me'
+
+
+class PissImages(models.Model):
+
+    class Meta:
+        db_table = "piss_images"
+
+    user_id = models.BigIntegerField()
+    qiniu_url = models.CharField(max_length=512)
+    piss_url = models.CharField(max_length=32, unique=True)
+    created_time = models.DateTimeField(auto_created=True, default=None)
+    updated_time = models.DateTimeField(auto_now=True)
+    is_deleted = models.BooleanField(default=False)
+
