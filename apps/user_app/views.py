8a4329f22c7208637b06684bf9170cb978a879ec
lightless <lightless@foxmail.com>
2016-11-29 23:17:18 +0800

    pause working...

--- apps/user_app/views.py
+++ apps/user_app/views.py
@@ -7,6 +7,12 @@ from django.shortcuts import render
 from django.views.decorators.csrf import csrf_exempt
 from django.utils.decorators import method_decorator
 from django.views import View
+from django.http import JsonResponse
+
+from .forms import RegisterForm
+from .models import PissUser
+from .models import PissUserExtra
+from .models import PissActiveCode
 
 __author__ = 'lightless'
 __email__ = 'root@lightless.me'
@@ -33,4 +39,18 @@ class RegisterView(View):
         return render(request, "user_app/register.html", context)
 
     def post(self, request):
-        pass
+
+        register_form = RegisterForm(request.POST)
+        if register_form.is_valid():
+            # 表单验证成功
+            # 1. 验证active code的可用性
+            active_code_qs = PissActiveCode.objects.filter()
+            # 2. 检查用户名和邮箱是否已经被注册
+            # 3. 更新active code状态， 插入用户数据，extra数据
+            # 4. 发送激活邮件
+            # 5. 返回成功信息
+            return JsonResponse(dict(code=1001, message=u"注册成功，请到邮箱检查激活邮件"))
+        else:
+            errors = register_form.errors
+            for e in errors:
+                return JsonResponse(dict(code=1004, message=errors[e][0]))
